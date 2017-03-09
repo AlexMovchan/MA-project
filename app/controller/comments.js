5d2b111868b19d2eca4b856bf9d399a32f4cef91
Your Name <you@example.com>
2017-03-09 12:13:26 +0200

    comments section

--- app/controller/comments.js
+++ app/controller/comments.js
@@ -1,54 +1,22 @@
 angular.module('myApp')
     .constant("baseUrl", "http://localhost:2403/my-comments/")
-    .controller('commentsCtrl', function($scope, $http, $resource, baseUrl){
-            // $scope.text = '';
-            // $scope.date = [];
-            // $scope.liked = false;
+    .controller('commentsCtrl', function($scope, $http, baseUrl){
 
-            // $scope.print = function() {
-            //     $scope.message.push({author: $scope.author, comment: $scope.text, like: false, date : new Date()});
-            //     $scope.text = '';
-            //     $scope.author ='';
-            // }
-            // $scope.changeLike = function(item) {
-            //     item.like = true;
-            //     item.$save();
-            // }
+            $scope.message = [];
 
+            $scope.refresh = function(){
+	            $http.get(baseUrl).then(function (data) {
+		            $scope.message = data;
+		        });
+        	}
 
-            //$scope.itemsResource = $resource(baseUrl + ":id", { id: "@id" });
-             $scope.message = [];
-            // $scope.refresh = function () {
-                $http.get(baseUrl).then(function (data) {
-                    $scope.message = data;
-                });
-            // }
-             // $scope.refresh();
-
-
-
-
-
+            $scope.addComent = function (item) {
+	            $http.post(baseUrl, item).then(function (item) {
+		            $scope.message.data.push(item);
+		        });
+        	}
            
 
-
-
-    /*
-            // редеактирование существующего или создание нового элемента
-            $scope.editOrCreate = function (item) {
-                $scope.currentItem = item ? angular.copy(item) : {};
-                $scope.currentView = "edit";
-            }
-
-            // сохранение изменений
-            $scope.saveEdit = function (item) {
-                // Если у элемента есть свойство id выполняем редактирование
-                // В данной реализации новые элементы не получают свойство id поэтому редактировать их невозможно (будет исправленно в слудующих примерах)
-                if (angular.isDefined(item.id)) {
-                    $scope.update(item);
-                } else {
-                    $scope.create(item);
-                }
-            }
-    */
+           
+        	$scope.refresh();
 })
--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
- angular.module('myApp', ['ngRoute', 'ngResource'])
+ angular.module('myApp', ['ngRoute'])
  	.config(function ($routeProvider, $locationProvider) {
 
       $locationProvider.html5Mode(true);
